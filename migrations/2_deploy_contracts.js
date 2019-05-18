b9bc1487155524c21a8c6f70adb48bb1a9a423cc
Thore Weilbier <thore@weilbier.net>
2019-05-18 17:48:33 +0200

    Tidy up default truffle box

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
-const TutorialToken = artifacts.require("TutorialToken");
-const ComplexStorage = artifacts.require("ComplexStorage");
+const TrashReward = artifacts.require('TrashReward')
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
-};
+  deployer.deploy(TrashReward)
+}
