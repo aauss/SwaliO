635b9edbb4791265ef3b1105e94c2a15c2d73986
Thore Weilbier <thore@weilbier.net>
2019-05-19 02:19:32 +0200

    Add QR code scanner

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,36 +1,51 @@
-import React, { useState } from "react";
-import { withRouter } from "react-router-dom";
-import { Box, Button, Heading, Text, TextInput } from "grommet";
+import React, { useState } from 'react'
+import { withRouter } from 'react-router-dom'
+import { Box, Button, Heading, Text, TextInput } from 'grommet'
+import QrReader from 'react-qr-reader'
 
-import { postToEndpoint } from "../helpers/fetch";
+import { postToEndpoint } from '../helpers/fetch'
 
 export default withRouter(({ history }) => {
-  const [loading, setLoading] = useState(false);
-  const [address, setAddress] = useState(null);
+  const [loading, setLoading] = useState(false)
+  const [address, setAddress] = useState(null)
 
   const handleClickGo = async () => {
-    setLoading(true);
-    await postToEndpoint("/start", { address })
+    setLoading(true)
+    await postToEndpoint('/start', { address })
     setTimeout(() => {
-      setLoading(false);
-      history.push("/process", { address })
-    }, 1000);
-  };
+      setLoading(false)
+      history.push('/process', { address })
+    }, 1000)
+  }
+
+  const handleScan = data => {
+    if (data) {
+      const address = data.slice(9)
+      setAddress(address)
+      handleClickGo()
+    }
+  }
+
+  const handleError = error => {
+    console.log(error)
+  }
 
   return (
-    <Box align={"center"} pad={"large"}>
-      <Heading>
-        Hello.
-      </Heading>
-      <Box pad={"large"}>
-        <Text size={"xlarge"}>
-          Enter your address to get started
-        </Text>
+    <Box align={'center'} pad={'large'}>
+      <Heading>Hello.</Heading>
+      <QrReader
+        delay={300}
+        onScan={handleScan}
+        onError={handleError}
+        style={{ width: '300px' }}
+      />
+      <Box pad={'large'}>
+        <Text size={'xlarge'}>Scan or enter your address to get started</Text>
       </Box>
-      <Box width={"large"}>
+      <Box width={'large'}>
         <TextInput
-          placeholder={"0x..."}
-          size={"xlarge"}
+          placeholder={'0x...'}
+          size={'xlarge'}
           value={address}
           onChange={event => setAddress(event.target.value)}
           disabled={loading}
@@ -38,9 +53,9 @@ export default withRouter(({ history }) => {
       </Box>
       <Button
         label={"Let's start"}
-        color={"accent-1"}
+        color={'accent-1'}
         primary
-        margin={"large"}
+        margin={'large'}
         onClick={handleClickGo}
         disabled={loading}
       />
