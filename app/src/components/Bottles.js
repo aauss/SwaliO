853e2adb0862ee1b9a5c8cde4f57aa6ac223360a
Thore Weilbier <thore@weilbier.net>
2019-05-19 02:24:23 +0200

    Fix balance update problem in front-end

--- app/src/components/Bottles.js
+++ app/src/components/Bottles.js
@@ -1,148 +1,121 @@
-import React, { useState, useEffect } from "react";
-import { withRouter } from "react-router-dom";
-import { Box, Button, Heading, Text } from "grommet";
+import React, { useState, useEffect } from 'react'
+import { withRouter } from 'react-router-dom'
+import { Box, Button, Heading, Text } from 'grommet'
 
-import { fetchFromEndpoint, postToEndpoint } from "../helpers/fetch";
+import { fetchFromEndpoint, postToEndpoint } from '../helpers/fetch'
 
 export default withRouter(({ history, location }) => {
-  const [loading, setLoading] = useState(false);
-  const [reward, setReward] = useState(0);
-  const [numberOfBottles, setNumberOfBottles] = useState(0);
-  const [balance, setBalance] = useState(0);
+  const [loading, setLoading] = useState(false)
+  const [reward, setReward] = useState(0)
+  const [numberOfBottles, setNumberOfBottles] = useState(0)
+  const [balance, setBalance] = useState(0)
 
-  const { address } = location.state;
-  
-  useEffect(() => {
-    async function fetchReward() {
-      const reward = await fetchFromEndpoint('/reward');
-      setReward(reward);
-    }
-    async function fetchBottles() {
-      const numberOfBottles = await fetchFromEndpoint('/bottles');
-      setNumberOfBottles(numberOfBottles);
-    }
-    async function fetchBalance() {
-      const balance = await fetchFromEndpoint(`/balance/${address}`);
-      setBalance(balance);
-    }
-    fetchReward();
-    fetchBalance();
-    const interval = setInterval(() => {
-      fetchBottles();
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [])
+  const { address } = location.state
+
+  useEffect(
+    () => {
+      async function fetchReward() {
+        const reward = await fetchFromEndpoint('/reward')
+        setReward(reward)
+      }
+      async function fetchBottles() {
+        const numberOfBottles = await fetchFromEndpoint('/bottles')
+        setNumberOfBottles(numberOfBottles)
+      }
+      async function fetchBalance() {
+        const balance = await fetchFromEndpoint(`/balance/${address}`)
+        setBalance(balance)
+      }
+      fetchReward()
+      fetchBalance()
+      const interval = setInterval(() => {
+        fetchBottles()
+      }, 1000)
+      return () => clearInterval(interval)
+    },
+    [location]
+  )
 
   const handleClickDone = async () => {
-    setLoading(true);
-    await postToEndpoint("/end", { address });
+    setLoading(true)
+    await postToEndpoint('/end', { address })
     setTimeout(() => {
-      setLoading(false);
-      history.push("/transaction", { address, totalReward: reward * numberOfBottles })
-    }, 1000);
+      setLoading(false)
+      history.push('/transaction', {
+        address,
+        totalReward: reward * numberOfBottles
+      })
+    }, 1000)
   }
 
   return (
-    <Box align={"center"} pad={"large"}>
-      <Heading>
-        Ready.
-      </Heading>
-      <Box pad={"large"}>
-        <Text size={"xlarge"}>
-          Throw in only WHITE glass!
-        </Text>
+    <Box align={'center'} pad={'large'}>
+      <Heading>Ready.</Heading>
+      <Box pad={'large'}>
+        <Text size={'xlarge'}>Throw in only WHITE glass!</Text>
       </Box>
       <Box
-        width={"medium"}
-        direction={"row"}
-        justify={"between"}
-        align={"center"}
-      >
-        <Text size={"large"}>
-          Address
-        </Text>
-        <Text size={"xlarge"} truncate margin={{ "left": "medium" }}>
+        width={'medium'}
+        direction={'row'}
+        justify={'between'}
+        align={'center'}>
+        <Text size={'large'}>Address</Text>
+        <Text size={'xlarge'} truncate margin={{ left: 'medium' }}>
           {address}
         </Text>
       </Box>
       <Box
-        width={"medium"}
-        direction={"row"}
-        justify={"between"}
-        margin={{ "top": "small"}}
-      >
-        <Text size={"large"}>
-          Balance
-        </Text>
-        <Text size={"xlarge"}>
-          {`${balance} ETH`}
-        </Text>
+        width={'medium'}
+        direction={'row'}
+        justify={'between'}
+        margin={{ top: 'small' }}>
+        <Text size={'large'}>Balance</Text>
+        <Text size={'xlarge'}>{`${balance} ETH`}</Text>
       </Box>
       <Box
-        direction={"row"}
-        width={"medium"}
+        direction={'row'}
+        width={'medium'}
         border={{
-          color: "dark-5",
-          size: "small",
-          side: "bottom"
+          color: 'dark-5',
+          size: 'small',
+          side: 'bottom'
         }}
-        margin={"medium"}
-      />     
+        margin={'medium'}
+      />
       <Box
-        width={"medium"}
-        direction={"row"}
-        justify={"between"}
-        margin={"small"}
-      >
-        <Text size={"large"}>
-          WHITE glass
-        </Text>
-        <Text size={"xlarge"}>
-          {`${numberOfBottles} x 🍼`}
-        </Text>
+        width={'medium'}
+        direction={'row'}
+        justify={'between'}
+        margin={'small'}>
+        <Text size={'large'}>WHITE glass</Text>
+        <Text size={'xlarge'}>{`${numberOfBottles} x 🍼`}</Text>
+      </Box>
+      <Box width={'medium'} direction={'row'} justify={'between'}>
+        <Text size={'large'}>Reward per bottle</Text>
+        <Text size={'xlarge'}>{reward} ETH</Text>
       </Box>
       <Box
-        width={"medium"}
-        direction={"row"}
-        justify={"between"}
-      >
-        <Text size={"large"}>
-          Reward per bottle
-        </Text>
-        <Text size={"xlarge"}>
-          {reward} ETH
-        </Text>
-      </Box>
-      <Box
-        direction={"row"}
-        width={"medium"}
+        direction={'row'}
+        width={'medium'}
         border={{
-          color: "dark-5",
-          size: "small",
-          side: "bottom"
+          color: 'dark-5',
+          size: 'small',
+          side: 'bottom'
         }}
-        margin={"medium"}
-      />        
-      <Box
-        width={"medium"}
-        direction={"row"}
-        justify={"between"}
-      >
-        <Text size={"xxlarge"}>
-          Total
-        </Text>
-        <Text size={"xxlarge"}>
-          {numberOfBottles * reward} ETH
-        </Text>
+        margin={'medium'}
+      />
+      <Box width={'medium'} direction={'row'} justify={'between'}>
+        <Text size={'xxlarge'}>Total</Text>
+        <Text size={'xxlarge'}>{numberOfBottles * reward} ETH</Text>
       </Box>
       <Button
-        label={"Done"}
-        color={"accent-1"}
+        label={'Done'}
+        color={'accent-1'}
         primary
-        margin={"large"}
+        margin={'large'}
         onClick={handleClickDone}
         disabled={loading}
       />
     </Box>
   )
-});
+})
