48fa2a68c6395241fcf8135d289b810b3745c7de
Thore Weilbier <thore@weilbier.net>
2019-05-19 04:28:48 +0200

    Prettify some stuff...

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,61 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Grommet } from "grommet";
-import { grommet } from "grommet/themes";
-import { DrizzleProvider } from "drizzle-react";
-import { LoadingContainer } from "drizzle-react-components";
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Grommet } from 'grommet'
+// import { grommet } from 'grommet/themes'
+import { DrizzleProvider } from 'drizzle-react'
+import { LoadingContainer } from 'drizzle-react-components'
 
 import './App.css'
 
-import Login from "./components/Login";
-import Bottles from "./components/Bottles";
-import Transaction from "./components/Transaction";
-import Map from './container/Map';
-import drizzleOptions from "./drizzleOptions"
+import Login from './components/Login'
+import Bottles from './components/Bottles'
+import Transaction from './components/Transaction'
+import Map from './container/Map'
+import drizzleOptions from './drizzleOptions'
+
+import logo from './images/logo.png'
+import background from './images/background.jpg'
+
+const baseStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'center',
+  minHeight: '100vh',
+  backgroundImage: `url(${background})`,
+  backgroundPosition: 'center center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover'
+}
+
+const imageStyle = {
+  minWidth: '450px',
+  display: 'flex',
+  justifyContent: 'center'
+}
+
+const colors = {}
+colors['accent-1'] = '#E87D10'
+
+const grommetTheme = {
+  global: {
+    colors,
+    font: {
+      weight: 'bold',
+      shadow: 'white 5px 5px'
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <Grommet theme={grommet}>
+      <Grommet theme={grommetTheme} style={baseStyle}>
+        {window.location.pathname !== '/map' && (
+          <div style={imageStyle}>
+            <img src={logo} alt="logo" height="500px" />
+          </div>
+        )}
         <DrizzleProvider options={drizzleOptions}>
           <LoadingContainer>
             <Router>
